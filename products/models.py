d1e2bd5483bad9a65634c6ee1b103c969c9bc7d2
Himanshu <himanshu1.sikarwar@ril.com>
2025-04-15 00:03:18 +0530

    migrations

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
+
+
 from django.db import models
+from django.utils.timezone import datetime
+
 
 # Create your models here.
+class products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    is_available = models.BooleanField(default= False)
+    created_at = models.DateTimeField(default= datetime.now)
